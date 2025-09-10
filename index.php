7bd2b6f39d27b874249f07582c4e1b29d5d649b9
Engineer-cat <estromberg224@gmail.com>
2025-09-10 14:26:13 +0200

    css kod och html grundlig

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bildsök</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div>
+
+        <div id="main">
+            <div class="main_container">
+                <div class="topbar"> 
+                    <input class="search_top" name="Sök" type="search" />
+                </div>
+
+                <div class="image_box side_margin">
+                    <div>
+                        <img src="bilder/waves.jpg">
+
+                    </div>
+                    <div>
+                        <img src="bilder/Landskapsbild.png">
+                    </div>
+                    <div>
+                        <img src="bilder/Katt.jpg">
+
+                    </div>
+                    <div>
+                        <img src="bilder/Fönsterbräda sktech sida.PNG">
+                    </div>
+                    <div>5</div>
+                    <div>6</div>
+                </div>
+            </div>
+            
+
+        </div>
+
+    </div>
+
+    <?php 
+    echo "Bild" . "\n";
+    echo "Php print"
+    
+    ?>
+</body>
+</html>
